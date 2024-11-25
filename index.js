1adecd4e50135840e2b0d36de519920675c67539
aladdin the coder <138482330+aladdincoder@users.noreply.github.com>
2024-11-25 20:58:43 +0600

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Welcome to Solar Titans", "Hola"];
+const statusMessages = ["Tatakae", "Tatakae"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
